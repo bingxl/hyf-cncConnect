22960195dfcee69134fa41ca017fcf227b116bf4
bingxl <scarecrowlxb@qq.com>
2026-07-27 08:31:03 +0800

    refactor: merge HistorySave into HistoryBrowse, fix empty-machine bugs
    - Remove HistorySavePage, merge save functionality into HistoryBrowsePage
    - Left panel: paginated batch list, Right panel: save + batch detail
    - Rename page label from '批次浏览' to '批次管理'
    - Remove UiPage::HistorySave enum value
    - Fix streaming_fetch stuck forever when machines list is empty
    - Fix start_save: show '没有机床数据' instead of saving empty batch

--- src/imgui_ui/view/HistoryView.hpp
+++ src/imgui_ui/view/HistoryView.hpp
@@ -2,19 +2,10 @@
 #include "core/IPage.hpp"
 #include "viewmodel/HistoryVm.hpp"
 
-class HistorySavePage : public IPage {
-public:
-    UiPage id() const override { return UiPage::HistorySave; }
-    const char* label() const override { return "保存批次"; }
-    void draw(AppState& state) override;
-private:
-    HistoryVm vm_;
-};
-
 class HistoryBrowsePage : public IPage {
 public:
     UiPage id() const override { return UiPage::HistoryBrowse; }
-    const char* label() const override { return "批次浏览"; }
+    const char* label() const override { return "批次管理"; }
     void draw(AppState& state) override;
 private:
     HistoryVm vm_;
@@ -35,6 +26,7 @@ private:
         bool is_batch = false;
     } del_;
 
+    void draw_save_panel();
     void draw_batch_list();
     void draw_batch_detail();
     void draw_edit_popup();
